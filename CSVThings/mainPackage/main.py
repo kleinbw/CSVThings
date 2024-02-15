af54d8da2b26e299cc09c277fea637ef64271018
kleinbw <kleinbw@vdi-LCB-IC-011.ad.uc.edu>
2024-02-15 08:10:52 -0500

    First commit to repo

--- CSVThings/mainPackage/main.py
+++ CSVThings/mainPackage/main.py
@@ -3,4 +3,16 @@
 from utilsPackage.utils import *
 
 # Call the function and store what it returns 
+if __name__ == "__main__":
+    data = read_CSV_file()
+    print("# of rows in dataset:", len(data))
+    print(data[0])
+    print(data[-1])
+    
+    #crash = data['Collison']
+    collisionTypes = {myData[6] for myData in data}
+    print(collisionTypes)
+    #print(type)
+    
+#help(csv)
 #  in a variable called data
